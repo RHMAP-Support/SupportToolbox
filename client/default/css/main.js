2f3f978290b8b9ef5c9cbfaf36fcb0930c40deb0
Brian Dooley <brian.dooley@feedhenry.com>
2014-02-25 20:14:48 +0000

    reartrange directory structure 2

---
+++ client/default/css/main.js
@@ -0,0 +1,15 @@
+$("#ClientBtn").click(function(){
+	// $("#wrapper").removeClass("hide").addClass("show");
+	$('#clientwrapper').show();
+	//$("#menuwrapper").removeClass("hide").addClass("show");
+	$("#cloudwrapper").addClass("hide");
+	
+});
+
+$("#CloudBtn").click(function(){
+	// $("#wrapper").removeClass("hide").addClass("show");
+	$('#cloudwrapper').show();
+	//$("#menuwrapper").removeClass("hide").addClass("show");
+	$("#clientwrapper").addClass("hide");
+	
+});
---
+++ client/default/js/jq.js
@@ -0,0 +1,9 @@
+$("#ClientBtn").click(function(){
+	//$("#menuwrapper").removeClass("hide").addClass("show");
+	$("#CloudBtn").addClass("hide");
+	
+});
+
+$("#CloudBtn").click(function(){
+	$("#ClientBtn").addClass("hide");
+});
